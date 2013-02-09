c2d0c4a0f052e037413695583686ddbcf1691834
hawaii <kai.noetzel@gmail.com>
2013-02-10 00:22:16 +0100

    Created a MyFuncs class and used register_instance to register all functions at once.

--- s_xmlrpc.py
+++ s_xmlrpc.py
@@ -4,30 +4,29 @@ port = 5544
 
 srv = SimpleXMLRPCServer(('0.0.0.0',port))
 
-def event(*args):
-	for arg in args:
-		print arg
-	return ''
+class MyFuncs:
 
-def listDevices(*args):
-	for arg in args:
-		print arg	
-	return ''
+	def event(*args):
+		for arg in args:
+			print arg
+		return ''
 
-def newDevices(*args):
-	for arg in args:
-		print arg
-	return ''
+	def listDevices(*args):
+		for arg in args:
+			print arg
+		return ''
 
-def newDevice(*args):
-	for arg in args:
-		print arg
-	return ''
+	def newDevices(*args):
+		for arg in args:
+			print arg
+		return ''
 
-srv.register_function(event)
-srv.register_function(listDevices)
-srv.register_function(newDevices)
-srv.register_function(newDevice)
+	def newDevice(*args):
+		for arg in args:
+			print arg
+		return ''
+
+srv.register_instance(MyFuncs())
 srv.register_multicall_functions()
 
 try:
